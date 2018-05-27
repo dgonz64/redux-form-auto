fd3e796af93c6a347f814348c68bf51b9bc09bae
David González <braben@gmail.com>
2018-05-27 10:28:50 +0100

    Export trModel

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ export { Autoform } from './ui/Autoform'
 export { InputWrap } from './ui/wrappers/InputWrap'
 export { InputArrayWrap } from './ui/wrappers/InputArrayWrap'
 export { translatable } from './validation'
-export { addTranslations, setTranslator, tr } from './translate'
+export {
+  addTranslations,
+  setTranslator,
+  tr,
+  trModel
+} from './translate'
 export { setLanguageByName } from './translation_utils'
 export {
   setSkin,
