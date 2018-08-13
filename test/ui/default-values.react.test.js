a1ea197a8ce72009d07d8a30f067e51948c197a2
David González <braben@gmail.com>
2018-08-13 18:45:53 +0100

    Test for default values

---
+++ test/ui/default-values.react.test.js
@@ -0,0 +1,40 @@
+import React, { PureComponent } from 'react'
+import renderer from 'react-test-renderer'
+import { mount } from 'enzyme'
+
+import { setLanguageByName } from '../../src/translation_utils'
+import config from './enzymeConfig'
+import {
+  Schema,
+  Autoform,
+  addSkinType,
+  InputWrap
+} from '../../src/index'
+
+import { BSInputWrapper } from '../../src/ui/components/BSInputWrapper'
+
+import { App } from './app'
+
+const defaulter = new Schema('defaulter', {
+  name: {
+    type: 'string',
+    defaultValue: 'tomato'
+  }
+})
+
+test('fields can have default values', () => {
+  const app = mount(
+    <App>
+      <Autoform schema={defaulter} />
+    </App>
+  )
+
+  console.log(app.html())
+
+  const form = app.find('form')
+  const inputs = form.find('input')
+
+  expect(inputs).toHaveLength(1)
+  const input = inputs.first()
+  expect(input.prop('value')).toBe('tomato')
+})
